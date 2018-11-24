50b2d3b60c926c46c43381bd76b130b19fc7ce09
jethrocabaluna <jethrocabaluna310@gmail.com>
2018-11-24 08:40:09 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function App(props) {
+    return (
+        <h1>Tattoo App</h1>
+    )
+};
---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const parentDir = path.join(__dirname, '../');
+
+module.exports = {
+    entry: [
+        path.join(parentDir, 'index.js')
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    output: {
+        path: parentDir + '/dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: parentDir,
+        historyApiFallback: true
+    }
+}
